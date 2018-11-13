6794045fc4e9c101467574f257d137490586eb47
zfp <zfp@umich.edu>
2018-11-12 20:55:20 -0500

    first commit

---
+++ mysite/secrets.py
@@ -0,0 +1,3 @@
+SECRET_KEY = '&75lvc!sja+hri4!a#1brh51qu3svtbjqpx9cm&tmt&7_g@^d_'
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY ='731662788200-et1dughco82oo60pur371ijc63dqa5tl.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET='AjJeHrJ9U2KecKyP_naS02U5'
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\fanpa\664\664\heritagesites\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
